2e04aaf1f4edfd014075ab8301aa1dcdb175dc3c
kanghansung <xovndrkd@naver.com>
2019-03-13 10:53:09 +0900

    Step1 Commit

---
+++ Step1/Assets/Script/AnchorController.cs
@@ -0,0 +1,131 @@
+﻿using GoogleARCore;
+using GoogleARCore.CrossPlatform;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AnchorController : NetworkBehaviour
+{
+    [SyncVar(hook = "_OnChangeId")]
+    private string CloudAnchorId = string.Empty;
+    private bool IsHost = false;
+    private bool ShouldResolve = false;
+    private CloudAnchorController CloudAnchorController;
+
+
+    public void Start()
+    {
+        CloudAnchorController = GameObject.Find("CloudAnchorController")
+                                                        .GetComponent<CloudAnchorController>();
+    }
+
+    public override void OnStartClient()
+    {
+        if (CloudAnchorId != string.Empty)
+        {
+            ShouldResolve = true;
+        }
+    }
+
+    public void Update()
+    {
+        if (ShouldResolve)
+        {
+            _ResolveAnchorFromId(CloudAnchorId);
+        }
+    }
+
+    [Command]
+    public void CmdSetCloudAnchorId(string cloudAnchorId)
+    {
+        CloudAnchorId = cloudAnchorId;
+    }
+
+    public string GetCloudAnchorId()
+    {
+        return CloudAnchorId;
+    }
+
+    public void HostLastPlacedAnchor(Component lastPlacedAnchor)
+    {
+        IsHost = true;
+
+#if !UNITY_IOS
+        var anchor = (Anchor)lastPlacedAnchor;
+#elif ARCORE_IOS_SUPPORT
+            var anchor = (UnityEngine.XR.iOS.UnityARUserAnchorComponent)lastPlacedAnchor;
+#endif
+
+#if !UNITY_IOS || ARCORE_IOS_SUPPORT
+        XPSession.CreateCloudAnchor(anchor).ThenAction(result =>
+        {
+            if (result.Response != CloudServiceResponse.Success)
+            {
+                Debug.Log(string.Format("Failed to host Cloud Anchor: {0}", result.Response));
+
+                //CloudAnchorController.OnAnchorHosted(false, result.Response.ToString());
+                return;
+            }
+
+            Debug.Log(string.Format("Cloud Anchor {0} was created and saved.", result.Anchor.CloudId));
+            CmdSetCloudAnchorId(result.Anchor.CloudId);
+
+            //CloudAnchorController.OnAnchorHosted(true, result.Response.ToString());
+        });
+#endif
+    }
+
+    private void _ResolveAnchorFromId(string cloudAnchorId)
+    {
+        CloudAnchorController.OnAnchorInstantiated(false);
+
+        // If device is not tracking, let's wait to try to resolve the anchor.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        ShouldResolve = false;
+
+        XPSession.ResolveCloudAnchor(cloudAnchorId).ThenAction((System.Action<CloudAnchorResult>)(result =>
+        {
+            if (result.Response != CloudServiceResponse.Success)
+            {
+                Debug.LogError(string.Format("Client could not resolve Cloud Anchor {0}: {1}",
+                                             cloudAnchorId, result.Response));
+
+                //CloudAnchorController.OnAnchorResolved(false, result.Response.ToString());
+                ShouldResolve = true;
+                return;
+            }
+
+            Debug.Log(string.Format("Client successfully resolved Cloud Anchor {0}.",
+                                    cloudAnchorId));
+
+            //CloudAnchorController.OnAnchorResolved(true, result.Response.ToString());
+            _OnResolved(result.Anchor.transform);
+        }));
+    }
+
+    
+
+    private void _OnResolved(Transform anchorTransform)
+    {
+        var cloudAnchorController = GameObject.Find("CloudAnchorController")
+                                              .GetComponent<CloudAnchorController>();
+        cloudAnchorController.SetWorldOrigin(anchorTransform);
+    }
+
+    /// <summary>
+    /// Callback invoked once the Cloud Anchor Id changes.
+    /// </summary>
+    /// <param name="newId">New identifier.</param>
+    private void _OnChangeId(string newId)
+    {
+        if (!IsHost && newId != string.Empty)
+        {
+            CloudAnchorId = newId;
+            ShouldResolve = true;
+        }
+    }
+
+}
---
+++ Step1/Assets/Script/CloudAnchorController.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class CloudAnchorController : MonoBehaviour
+{
+    public UIController UIController;
+    public GameObject ARCoreRoot;
+    public ARCoreWorldOriginHelper ARCoreWorldOriginHelper;
+    private ApplicationMode m_CurrentMode = ApplicationMode.Ready;
+    private bool IsQuitting = false;
+    private Component LastPlacedAnchor = null;
+    private bool IsOriginPlaced = false;
+    private bool AnchorAlreadyInstantiated = false;
+
+    
+
+
+
+
+    public enum ApplicationMode
+    {
+        Ready,
+        Hosting,
+        Resolving,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.name = "CloudAnchorController";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _UpdateApplicationLifecycle();
+
+        if (m_CurrentMode != ApplicationMode.Hosting && m_CurrentMode != ApplicationMode.Resolving)
+        {
+            return;
+        }
+
+        // If the origin anchor has not been placed yet, then update in resolving mode is complete.
+        if (m_CurrentMode == ApplicationMode.Resolving && !IsOriginPlaced)
+        {
+            return;
+        }
+
+        // If the player has not touched the screen then the update is complete.
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+
+        TrackableHit hit;
+        if (ARCoreWorldOriginHelper.Raycast(touch.position.x, touch.position.y,
+                TrackableHitFlags.PlaneWithinPolygon, out hit))
+        {
+            LastPlacedAnchor = hit.Trackable.CreateAnchor(hit.Pose);
+        }
+
+        else if (!IsOriginPlaced && m_CurrentMode == ApplicationMode.Hosting)
+        {
+            SetWorldOrigin(LastPlacedAnchor.transform);
+            _InstantiateAnchor();
+            OnAnchorInstantiated(true);
+        }
+
+
+
+    }
+
+
+    public void SetWorldOrigin(Transform anchorTransform)
+    {
+        if (IsOriginPlaced)
+        {
+            Debug.LogWarning("The World Origin can be set only once.");
+            return;
+        }
+
+        IsOriginPlaced = true;    
+        ARCoreWorldOriginHelper.SetWorldOrigin(anchorTransform);
+    
+    }
+
+    private void _InstantiateAnchor()
+    {
+        // The anchor will be spawned by the host, so no networking Command is needed.
+        GameObject.Find("LocalPlayer").GetComponent<PlayerController>()
+                  .SpawnPlayer(Vector3.zero, Quaternion.identity, LastPlacedAnchor);
+    }
+
+    public void OnEnterHostingModeClick()
+    {
+        if (m_CurrentMode == ApplicationMode.Hosting)
+        {
+            m_CurrentMode = ApplicationMode.Ready;
+            _ResetStatus();
+            return;
+        }
+
+        m_CurrentMode = ApplicationMode.Hosting;
+        
+    }
+
+    public void OnEnterResolvingModeClick()
+    {
+        if (m_CurrentMode == ApplicationMode.Resolving)
+        {
+            m_CurrentMode = ApplicationMode.Ready;
+            _ResetStatus();
+            return;
+        }
+
+        m_CurrentMode = ApplicationMode.Resolving;
+        
+    }
+
+    private void _ResetStatus()
+    {
+        // Reset internal status.
+        m_CurrentMode = ApplicationMode.Ready;
+        if (LastPlacedAnchor != null)
+        {
+            Destroy(LastPlacedAnchor.gameObject);
+        }
+
+        LastPlacedAnchor = null;
+    }
+
+    public void OnAnchorInstantiated(bool isHost)
+    {
+        if (AnchorAlreadyInstantiated)
+        {
+            return;
+        }
+
+        AnchorAlreadyInstantiated = true;
+    }
+
+
+
+
+    //기본 설정 맞추는 메소드들 3개
+    private void _UpdateApplicationLifecycle()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        var sleepTimeout = SleepTimeout.NeverSleep;
+
+#if !UNITY_IOS
+        // Only allow the screen to sleep when not tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            const int lostTrackingSleepTimeout = 15;
+            sleepTimeout = lostTrackingSleepTimeout;
+        }
+#endif
+
+        Screen.sleepTimeout = sleepTimeout;
+
+        if (IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            _ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
+            IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+    }
+
+    /// <summary>
+    /// Actually quit the application.
+    /// </summary>
+    private void _DoQuit()
+    {
+        Application.Quit();
+    }
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
+                    message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+
+}
